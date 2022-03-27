a5811cafdcad8c3696ffbf85138003b5a23145b2
AleksandrTsygulev <aleksandr.tsygulev@gmail.com>
2022-03-27 14:06:27 +0300

    Initial commit

---
+++ src/main/java/com/example/domashka2/Domashka2Application.java
@@ -0,0 +1,13 @@
+package com.example.domashka2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Domashka2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Domashka2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/domashka2/Domashka2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.domashka2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Domashka2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
